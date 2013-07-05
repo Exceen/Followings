5f5f90ca01f2c47b0a3da781b4b6c5e2461d7eeb
Exceen <d11001@me.com>
2013-07-05 10:04:34 +0200

    Update followings.py

--- followings.py
+++ followings.py
@@ -2,19 +2,44 @@
 # (c) 2013 Exceen
 
 import tweepy
+import argparse
 from os.path import exists
 
-auth = tweepy.OAuthHandler('Consumer key', 'Consumer secret')
-auth.set_access_token('Access token', 'Access token secret')
-api = tweepy.API(auth)
+try:
+	auth = tweepy.OAuthHandler('Consumer key', 'Consumer secret')
+	auth.set_access_token('Access token', 'Access token secret')
+	api = tweepy.API(auth)
+	api.me()
+except tweepy.error.TweepError, err:
+	print err.reason
+	if 'invalid oauth token' in err.reason:
+		print 'Please check you consumer and access keys!'
+	exit()
 
 def main():
-	followers = api.followers_ids()
+
+	followers_file = 'followers'
+
+	parser = argparse.ArgumentParser(description='Followings v1.1')
+	parser.add_argument('-f', '--followers', action='store_true', help='lists all followers')
+	parser.add_argument('user', metavar='username', type=str, nargs='?', help='use another username instead of yours')
+	args = parser.parse_args()
+
+	user = api.me().screen_name
+	if args.user:
+		user = args.user
+		followers_file += '.' + user
+
+	followers = api.followers_ids(user)
 	print 'You have currently %d followers.' % len(followers)
 
-	if exists('followers.txt'):
+	if args.followers:
+		printUsers(followers)
+		exit()
 
-		recent_followers = [line.strip() for line in open('followers.txt')]
+
+	if exists(followers_file):
+		recent_followers = [line.strip() for line in open(followers_file)]
 
 		current_followers = []
 		for user in followers:
@@ -25,7 +50,7 @@ def main():
 
 		print '\nunfollowers:'
 		printUsers(unfollowers)
-			
+
 		print '\nnew followers:'
 		printUsers(new_followers)
 
@@ -33,14 +58,14 @@ def main():
 
 	else:
 		print 'I just created a file so that I have something to compare when you check your followings next time.'
-		
-	writeFollowersToFile(followers)
 
-def writeFollowersToFile(followers_list):
-	followers_file = open('followers.txt', 'w')
+	writeFollowersToFile(followers, followers_file)
+
+def writeFollowersToFile(followers_list, followers_file):
+	file_to_write = open(followers_file, 'w')
 	for user in followers_list:
-		followers_file.write('%d\n' % user)
-	followers_file.close()
+		file_to_write.write('%d\n' % user)
+	file_to_write.close()
 
 def printUsers(user_id_list):
 	if len(user_id_list) == 0:
